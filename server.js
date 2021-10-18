0f1205dbf41834b03c3c2e67263f7afd91f7e0ce
Akash <akashkothawade77@gmail.com>
2021-10-18 20:34:35 +0530

    basics routes working

--- server.js
+++ server.js
@@ -2,8 +2,23 @@
 
 const express = require('express');
 const app = express();
-
 const port = process.env.port || 7000;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+	res.status(201).json({ message: 'hello' });
+});
+
+app.post('/', (req, res) => {
+	try {
+		const newUser = {
+			id: req.body.id,
+			name: req.body.name,
+		};
+		res.json(newUser);
+	} catch (error) {}
+});
+
 app.listen(port, () => {
 	console.log(`app is running on ${port}`);
 });
